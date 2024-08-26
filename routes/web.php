3383b1a0139e0c000205d07c180eb9fbc32864d9
pmorenoq <pmorenoq@eafit.edu.co>
2024-08-26 09:54:22 -0500

    tutorial2

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,29 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+// toda ruta debe estar vinculada a un metodo de algun controlador
+// todas las rutas deben ser String-based
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+
+// siemore que se envien datos a las vistas se tienen que enviar con un arreglo asociativo
+// de tipo $viewData   
+
 Route::get('/about', function () {
-    $data1 = "About us - Online Store";
-    $data2 = "About us";
-    $description = "This is an about page ...";
-    $author = "Developed by: Pablo Moreno";
-    return view('home.about')->with("title", $data1)
-      ->with("subtitle", $data2)
-      ->with("description", $description)
-      ->with("author", $author);
-})->name("home.about");
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+    $data1 = 'About us - Online Store';
+    $data2 = 'About us';
+    $description = 'This is an about page ...';
+    $author = 'Developed by: Pablo Moreno';
 
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
+    return view('home.about')->with('title', $data1)
+        ->with('subtitle', $data2)
+        ->with('description', $description)
+        ->with('author', $author);
+})->name('home.about');
 
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
